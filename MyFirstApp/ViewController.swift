3032c83ba905ae8798eaa1c8caa44a7d650e2c36
Edric Chan <edric.chan.1997@gmail.com>
2016-07-20 21:10:56 +0800

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Chan Zi Yang, Edric on 20/7/16.
+//  Copyright © 2016 Studio10. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
