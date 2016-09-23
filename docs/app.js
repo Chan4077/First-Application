f7b9dd078f9da5fa8ac2f5d40763cf2af11ee15d
Edric Chan <edric.chan.1997@gmail.com>
2016-09-23 18:14:58 +0800

    Create app.js

---
+++ docs/app.js
@@ -0,0 +1,8 @@
+var app = angular.module('MyFirstApp', ['ngMaterial']);
+// Config
+app.config(function() {
+  console.info('Successfully initialized!');
+});
+app.controller('MainController', function() {
+
+});
