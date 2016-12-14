21d0a202d8193f7a4b4e5d47c1ca93a16dba83f2
Edric Chan <edric.chan.1997@gmail.com>
2016-12-14 18:42:45 +0800

    feat(dependencies.js) - Import dependencies.js for use

--- docs/app.js
+++ docs/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('MyFirstApp', ['ngAnimate', 'ngAria', 'ngMaterial', 'ngMessages']);
+var app = angular.module('MyFirstApp', ['ngAnimate', 'ngAria', 'ngMaterial', 'ngMessages', 'app.config', 'app.directives']);
 // Config
 app.config(function($log) {
   $log.info('Successfully initialized!');
@@ -8,42 +8,4 @@ app.controller('MainController', function($scope) {
 });
 app.controller('SideNavController', function ($scope, $mdSidenav) {
     console.info('SideNavController successfully loaded!');
-
-    $scope.main = [
-        { url: 'https://chan4077.github.io', title: 'Main', icon: 'home' },
-        { url: 'https://chan4077.github.io/about', title: 'About Me', icon: 'account-box' },
-        { url: 'https://chan4077.github.io/blog', title: 'Blog', icon: 'blogger' },
-        { url: 'https://chan4077.github.io/media', title: 'Social Media', icon: 'google-plus' },
-        { url: 'https://chan4077.github.io/angular', title: 'Angular', icon: 'angular' },
-    ];
-    $scope.other = [
-        { url: 'https://github.com/Chan4077/chan4077.github.io', title: 'Project Page', icon: 'github-circle' },
-        { url: 'https://chan4077.github.io/preferences', title: 'Preferences', icon: 'settings' },
-        { url: 'https://chan4077.github.io/help', title: 'Get Help', icon: 'help-circle' }
-    ];
-    $scope.projects = [
-        { url: 'https://chan4077.github.io/projects', title: 'Projects', icon: ''},
-        { url: 'https://chan4077.github.io/MyFirstApp', title: 'MyFirstApp', icon: ''},
-    ]
-    $scope.openLeftMenu = function () {
-        $mdSidenav('left').toggle();
-    }
-});
-/*
-*  Directives
-*/
-// Sidenav
-app.directive('siteSidenav', function () {
-    return {
-        restrict: 'E',
-        templateUrl: '/templates/sidenav.html',
-        transclude: true
-    };
-});
-app.directive('siteToolbar', function () {
-    return {
-        restrict: 'E',
-        templateUrl: '/templates/toolbar.html',
-        transclude: true
-    };
 });
